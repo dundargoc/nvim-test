51ee12d18990ba7212748009285daf309a94386c
dundargoc <gocdundar@gmail.com>
2025-04-12 16:03:05 +0200

    fixup: quick update, squash later

--- lua/nvim-test/busted/runner.lua
+++ lua/nvim-test/busted/runner.lua
@@ -6,5 +6,4 @@ package.path = (prefix .. 'share/lua/5.1/?.lua;')
 local platform = vim.uv.os_uname()
 local suffix = (platform and platform.sysname:lower():find 'windows') and '.dll' or '.so'
 package.cpath = prefix .. 'lib/lua/5.1/?' .. suffix .. ';' .. package.cpath
-print(package.cpath)
 require('busted.runner')({ standalone = false })
