fe75fbc0ad3b03f494be6268305c751bb516c113
dundargoc <gocdundar@gmail.com>
2025-04-12 16:02:49 +0200

    fixup: quick update, squash later

--- lua/nvim-test/busted/runner.lua
+++ lua/nvim-test/busted/runner.lua
@@ -1,14 +1,10 @@
-local extra_path = os.getenv('XDG_DATA_HOME').. '/nvim-test/luarocks/share/lua/5.1/'
-package.path = (extra_path .. '?.lua;')
-  .. (extra_path .. '?/init.lua;')
+local prefix = os.getenv('XDG_DATA_HOME') .. '/nvim-test/luarocks/'
+package.path = (prefix .. 'share/lua/5.1/?.lua;')
+  .. (prefix .. 'share/lua/5.1/?/init.lua;')
   .. package.path
--- extra_path= os.getenv('HOME').. '/.luarocks/share/lua/5.1/'
--- package.path = (extra_path .. '?.lua;')
---   .. (extra_path .. '?/init.lua;')
---   .. package.path
 
 local platform = vim.uv.os_uname()
 local suffix = (platform and platform.sysname:lower():find 'windows') and '.dll' or '.so'
-package.cpath = os.getenv('XDG_DATA_HOME') .. '/nvim-test/luarocks/lib/lua/5.1/?' .. suffix .. ';' .. package.cpath
+package.cpath = prefix .. 'lib/lua/5.1/?' .. suffix .. ';' .. package.cpath
 print(package.cpath)
 require('busted.runner')({ standalone = false })
