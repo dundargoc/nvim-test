7b18cb7ebe42a5feaf007eaa1440274bc2de64a8
dundargoc <gocdundar@gmail.com>
2025-04-12 15:53:01 +0200

    fixup: quick update, squash later

--- lua/nvim-test/busted/runner.lua
+++ lua/nvim-test/busted/runner.lua
@@ -2,7 +2,7 @@ local extra_path = os.getenv('XDG_DATA_HOME').. '/nvim-test/luarocks/share/lua/5
 package.path = (extra_path .. '?.lua;')
   .. (extra_path .. '?/init.lua;')
   .. package.path
-extra_path= os.getenv('HOME').. '.luarocks/luarocks/share/lua/5.1/'
+extra_path= os.getenv('HOME').. '.luarocks/share/lua/5.1/'
 package.path = (extra_path .. '?.lua;')
   .. (extra_path .. '?/init.lua;')
   .. package.path
