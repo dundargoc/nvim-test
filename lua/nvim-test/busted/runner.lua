136d9552ec340a4ca55d1cd4dc0b9f623a8052ca
dundargoc <gocdundar@gmail.com>
2025-04-12 15:59:33 +0200

    fixup: quick update, squash later

--- lua/nvim-test/busted/runner.lua
+++ lua/nvim-test/busted/runner.lua
@@ -6,5 +6,9 @@ package.path = (extra_path .. '?.lua;')
 -- package.path = (extra_path .. '?.lua;')
 --   .. (extra_path .. '?/init.lua;')
 --   .. package.path
+
+local platform = vim.uv.os_uname()
+local suffix = (platform and platform.sysname:lower():find 'windows') and '.dll' or '.so'
+package.cpath = os.getenv(XDG_DATA_HOME) .. '/lib/lua/5.1/?' .. suffix .. ';' .. package.cpath
 print(package.cpath)
 require('busted.runner')({ standalone = false })
