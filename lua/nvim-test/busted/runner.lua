c10dc4783eeaf17799a7a06f0a3eb470efd98822
dundargoc <gocdundar@gmail.com>
2025-04-12 15:32:38 +0200

    fixup: quick update, squash later

--- lua/nvim-test/busted/runner.lua
+++ lua/nvim-test/busted/runner.lua
@@ -1,6 +1,6 @@
 local datadir = os.getenv('XDG_DATA_HOME')
-package.path = (datadir .. '/nvim-test/luarocks/?.lua;')
-  .. (datadir .. '/nvim-test/?/init.lua;')
+package.path = (datadir .. '/nvim-test/luarocks/share/?.lua;')
+  .. (datadir .. '/nvim-test/luarocks/share/?/init.lua;')
   .. package.path
 print(package.path)
 require('busted.runner')({ standalone = false })
